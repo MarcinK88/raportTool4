cffe96364863e84ff9e1c8726a40ae94b36bb7e6
Marcin <marcin3216@gmail.com>
2019-08-26 15:28:20 +0200

    queries changed for postgresql

--- src/main/java/pl/marcin/raportTool4/Repositories/ConvertedRepository.java
+++ src/main/java/pl/marcin/raportTool4/Repositories/ConvertedRepository.java
@@ -25,11 +25,11 @@ public interface ConvertedRepository extends JpaRepository<Converted, Long> {
     int countByRequestTypeAndOpenDateStartsWithAndBa(String requestType, int year, int month, String ba);
 
 //    @Query(value="select COUNT(*) from converted where `BA` = ?1 AND YEAR(`Open date`) = ?2 AND MONTH(`Open date`) = ?3", nativeQuery = true)
-    @Query(value = "select COUNT(*) from converted where \"BA\" =?1 and EXTRACT(year from \"open date\") = ?2 AND EXTRACT(month from \"open date\")=?3", nativeQuery = true)
+    @Query(value = "select COUNT(*) from converted where \"ba\" =?1 and EXTRACT(year from \"open date\") = ?2 AND EXTRACT(month from \"open date\")=?3", nativeQuery = true)
     int countRequestPerBa(String ba, int year, int month);
 
 //    @Query(value="select COUNT(*) from converted where `Region` = ?1 AND YEAR(`Open date`) = ?2 AND MONTH(`Open date`) = ?3", nativeQuery = true)
-    @Query(value = "select COUNT(*) from converted where \"Region\" =?1 and EXTRACT(year from \"open date\") = ?2 AND EXTRACT(month from \"open date\")=?3", nativeQuery = true)
+    @Query(value = "select COUNT(*) from converted where \"region\" =?1 and EXTRACT(year from \"open date\") = ?2 AND EXTRACT(month from \"open date\")=?3", nativeQuery = true)
     int countRequestPerRegion(String region, int year, int month);
 
 //    @Query(value = "select COUNT(*) from converted where `Request type` = ?1 AND `Open date` >= ?2 AND `Open date` < ?3", nativeQuery = true)
@@ -41,16 +41,16 @@ public interface ConvertedRepository extends JpaRepository<Converted, Long> {
     double kpi1(Date begin, Date end);
 
 //    @Query(value = "select ROUND(( (select COUNT(*) from converted where `Request status` = \"closed\" AND `Resolution Time (in days)` = 1 AND `Open date` >= ?1 and `Open date` < ?2) * 100 / (select COUNT(*) from converted where `Request status` = \"closed\" AND `Resolution Time (in days)` is not null AND `Open date` >= ?1 and `Open date` < ?2)),1)", nativeQuery = true)
-    @Query(value = "select ROUND(( (select COUNT(*) from converted where \"Request status\" = 'closed' AND \"Resolution Time (in days)\" = 1 AND \"Open date\" >= ?1 and \"Open date\" < ?2) * 100 / (select COUNT(*) from converted where \"Request status\" = 'closed' AND \"Resolution Time (in days)\" is not null AND \"Open date\" >= ?1 and \"Open date\" < ?2)),1)", nativeQuery = true)
+    @Query(value = "select ROUND(( (select COUNT(*) from converted where \"request status\" = 'closed' AND \"resolution time (in days)\" = 1 AND \"open date\" >= ?1 and \"open date\" < ?2) * 100 / (select COUNT(*) from converted where \"request status\" = 'closed' AND \"resolution time (in days)\" is not null AND \"open date\" >= ?1 and \"open date\" < ?2)),1)", nativeQuery = true)
     double kpi2(Date begin, Date end);
 
 //    @Query(value = "select ROUND(AVG(`Resolution Time (in days)`),1) from converted where `Request type` = ?1 AND `Open date` >= ?2 and `Open date` < ?3", nativeQuery = true)
-    @Query(value = "select ROUND(AVG(\"Resolution Time (in days)\"),1) from converted where \"Request type\" = ?1 AND \"Open date\" >= ?2 and \"Open date\" < ?3", nativeQuery = true)
+    @Query(value = "select ROUND(AVG(\"resolution time (in days)\"),1) from converted where \"request type\" = ?1 AND \"open date\" >= ?2 and \"open date\" < ?3", nativeQuery = true)
     double kpi3(String type, Date begin, Date end);
 
     @Transactional
     @Modifying
-    @Query(value = "update converted set \"Open date\" = ?2, \"Close date\" = ?3  where \"id\" = ?1", nativeQuery = true)
+    @Query(value = "update converted set \"open date\" = ?2, \"close date\" = ?3  where \"id\" = ?1", nativeQuery = true)
     void setDates(long id, Date openDate, Date closeDate);
 
 }
